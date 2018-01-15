4d75c50a465bc0ad8845ee59e9e358ec7b7654ba
tinker <tinkerscript@yandex.ru>
2018-01-15 22:30:37 +0500

    update hello_world; add send.sql demo

---
+++ demo/01_hello_world/send.sql
@@ -0,0 +1,12 @@
+BEGIN TRAN;
+
+DECLARE @DialogId UNIQUEIDENTIFIER;
+
+BEGIN DIALOG @DialogId
+FROM SERVICE [//sqlssb/demo_service_1] TO SERVICE '//sqlssb/demo_service_2'
+ON CONTRACT [//sqlssb/demo_contract] WITH ENCRYPTION=OFF; 
+
+SEND ON CONVERSATION @DialogId
+MESSAGE TYPE [//sqlssb/demo_message] ('hello, broker!');
+
+COMMIT TRAN;
