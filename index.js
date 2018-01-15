8891e8fb796046decc61c179d24af0202dc8403d
tinker <tinkerscript@yandex.ru>
2018-01-15 22:19:36 +0500

    extends Sqlssb with EventEmitter

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-module.exports = class Sqlssb {
+const EventEmitter = require('events')
+
+module.exports = class Sqlssb extends EventEmitter {
   constructor (config) {
+    super()
     this._config = config
   }
 
   listen (port, options) {
     console.log(`listening in port [${port}]`)
-  }
-
-  on (messageType, handler) {
-    //
   }
 }
