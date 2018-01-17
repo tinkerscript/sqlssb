ed3758cff59fa7e1e953d85996f767f69f6fe9cf
tinker <tinkerscript@yandex.ru>
2018-01-17 22:08:21 +0500

    update hello_world send script

--- demo/01_hello_world/send.sql
+++ demo/01_hello_world/send.sql
@@ -3,7 +3,7 @@ BEGIN TRAN;
 DECLARE @DialogId UNIQUEIDENTIFIER;
 
 BEGIN DIALOG @DialogId
-FROM SERVICE [//sqlssb/demo_service_1] TO SERVICE '//sqlssb/demo_service_2'
+FROM SERVICE [//sqlssb/demo_service_2] TO SERVICE '//sqlssb/demo_service_1'
 ON CONTRACT [//sqlssb/demo_contract] WITH ENCRYPTION=OFF; 
 
 SEND ON CONVERSATION @DialogId
