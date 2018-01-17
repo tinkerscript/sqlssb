92a57f9d687b0154d663eade791e1f7976d2d1cc
tinker <tinkerscript@yandex.ru>
2018-01-17 21:54:03 +0500

    rename demo app to service1

--- demo/01_hello_world/index.js
+++ demo/01_hello_world/index.js
@@ -1,6 +1,6 @@
 const Sqlssb = require('../../source')
 const config = require('./config.json')
-const app = new Sqlssb(config)
+const service1 = new Sqlssb(config)
 
-app.on('//sqlssb/demo_message', console.log)
-app.start()
+service1.on('//sqlssb/demo_message', console.log)
+service1.start()
