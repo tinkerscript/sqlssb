9846cb835777aac7bb40450dbff80ec629778111
tinker <tinkerscript@yandex.ru>
2018-01-18 23:11:26 +0500

    hello, broker! -> hello, world!

--- demo/01_hello_world/send.sql
+++ demo/01_hello_world/send.sql
@@ -7,6 +7,6 @@ FROM SERVICE [//sqlssb/demo_service_2] TO SERVICE '//sqlssb/demo_service_1'
 ON CONTRACT [//sqlssb/demo_contract] WITH ENCRYPTION=OFF; 
 
 SEND ON CONVERSATION @DialogId
-MESSAGE TYPE [//sqlssb/demo_message] ('hello, broker!');
+MESSAGE TYPE [//sqlssb/demo_message] ('hello, world!');
 
 GO
