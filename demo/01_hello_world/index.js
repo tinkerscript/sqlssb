bfdc6754e97be36931884b209fa98551a0de0727
tinker <tinkerscript@yandex.ru>
2018-01-21 22:44:45 +0500

    rollback and fix hello world demo

--- demo/01_hello_world/index.js
+++ demo/01_hello_world/index.js
@@ -1,16 +1,9 @@
 const Sqlssb = require('../../source')
-const config1 = require('../service1.config.json')
-const config2 = require('../service1.config.json')
-const service1 = new Sqlssb(config1)
-const service2 = new Sqlssb(config2)
+const config = require('../service1.config.json')
+const service1 = new Sqlssb(config)
 
 service1.on('//sqlssb/demo_message', ctx => {
   console.log(ctx.message_body)
 })
 
-service2.on('//sqlssb/demo_message', ctx => {
-  console.log(ctx.message_body)
-})
-
 service1.start()
-service2.start()
