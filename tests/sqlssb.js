0e22cf0f5c736df9d7ce2aa6eb95a5d9deb3c914
tinker <tinkerscript@yandex.ru>
2018-01-24 23:44:47 +0500

    unit tests infrastructure

---
+++ tests/sqlssb.js
@@ -0,0 +1,9 @@
+/* eslint-env mocha */
+const assert = require('assert')
+const Sqlssb = require('../source')
+
+describe('sqlssb', () => {
+  it('should be ok', () => {
+    assert.ok(Sqlssb)
+  })
+})
