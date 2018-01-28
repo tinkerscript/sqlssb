8321b70b43b8d31b9d4b3d161e204270ac32b767
tinker <tinkerscript@yandex.ru>
2018-01-28 22:16:33 +0500

    messaging test dummy

--- tests/sqlssb.js
+++ tests/sqlssb.js
@@ -3,7 +3,17 @@ const assert = require('assert')
 const Sqlssb = require('../source')
 
 describe('sqlssb', () => {
-  it('should be ok', () => {
-    assert.ok(Sqlssb)
+  it('should provide messaging', () => {
+    const service1 = new Sqlssb({})
+    const service2 = new Sqlssb({})
+
+    service1.connect()
+    service2.connect()
+
+    service1.on('sample-message-type', ({ messageBody }) => {
+      assert.equal(messageBody, 'test')
+    })
+
+    service2.send('sample-message-type', 'test')
   })
 })
