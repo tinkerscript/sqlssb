7241ba9839eb07eb756a8810a20dd232c06741fd
tinker <tinkerscript@yandex.ru>
2018-01-29 19:51:29 +0500

    simple dialog test (failing)

--- tests/sqlssb.js
+++ tests/sqlssb.js
@@ -31,4 +31,42 @@ describe('sqlssb', () => {
     service1.start()
     service2.send('sqlssb1', 'sample-message-type', 'hello')
   })
+
+  it('should provide dialog', () => {
+    const adapter = new FakeDataAdapter()
+    const service1 = new Sqlssb({ adapter })
+    const service2 = new Sqlssb({ adapter })
+
+    return new Promise(resolve => {
+      const dialog = []
+
+      service1.on('sample-message-type', ctx => {
+        const { messageBody } = ctx
+        dialog.push(messageBody)
+
+        if (messageBody === 'well hello') {
+          ctx.reply('sample-message-type', 'bye')
+        }
+      })
+
+      service2.on('sample-message-type', ctx => {
+        const { messageBody } = ctx
+        dialog.push(messageBody)
+
+        if (messageBody === 'hello') {
+          ctx.reply('sample-message-type', 'well hello')
+        }
+
+        if (messageBody === 'bye') {
+          resolve(dialog)
+        }
+      })
+
+      service1.start()
+      service2.start()
+      service1.send('sqlssb2', 'sample-message-type', 'hello')
+    }).then(dialog => {
+      assert.deepEqual(dialog, ['hello', 'well hello', 'bye'])
+    })
+  })
 })
