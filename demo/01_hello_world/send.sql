f7bfd6a8e846a1c25610ae437a64487b4aff48ce
tinker <tinkerscript@yandex.ru>
2018-02-03 21:44:32 +0500

    fix sample db objects names in sql scripts

--- demo/01_hello_world/send.sql
+++ demo/01_hello_world/send.sql
@@ -3,7 +3,7 @@ USE sqlssb;
 DECLARE @DialogId UNIQUEIDENTIFIER;
 
 BEGIN DIALOG @DialogId
-FROM SERVICE [//sqlssb/demo_service_2] TO SERVICE '//sqlssb/demo_service_1'
+FROM SERVICE [sqlssb_demo_service_2] TO SERVICE 'sqlssb_demo_service_1'
 ON CONTRACT [//sqlssb/demo_contract] WITH ENCRYPTION=OFF; 
 
 SEND ON CONVERSATION @DialogId
--- demo/reset.sql
+++ demo/reset.sql
@@ -15,10 +15,10 @@ CREATE CONTRACT [//sqlssb/demo_contract] (
 CREATE QUEUE sqlssb_demo_queue_1;
 CREATE QUEUE sqlssb_demo_queue_2;
 
-CREATE SERVICE [//sqlssb/demo_service_1]
+CREATE SERVICE [sqlssb_demo_service_1]
   ON QUEUE sqlssb_demo_queue_1 ([//sqlssb/demo_contract]);
 
-CREATE SERVICE [//sqlssb/demo_service_2]
+CREATE SERVICE [sqlssb_demo_service_2]
   ON QUEUE sqlssb_demo_queue_2 ([//sqlssb/demo_contract]);
 
 GO
