4cfe8147c9096d2748f7bf8bd371db60b079f57f
tinker <tinkerscript@yandex.ru>
2018-02-03 21:45:58 +0500

    fix and tweak dialog demo

--- demo/02_simple_dialog/index.js
+++ demo/02_simple_dialog/index.js
@@ -1,11 +1,8 @@
 const Sqlssb = require('../../source')
-const config1 = require('../service1.config.json')
-const config2 = require('../service1.config.json')
-const service1 = new Sqlssb(config1)
-const service2 = new Sqlssb(config2)
+const service1 = new Sqlssb(require('../service1.config.json'))
+const service2 = new Sqlssb(require('../service2.config.json'))
 
 service1.on('//sqlssb/demo_message', ctx => {
-  console.log(ctx.messageBody)
   if (ctx.messageBody === 'STOP') {
     service1.stop()
     return
@@ -14,23 +11,22 @@ service1.on('//sqlssb/demo_message', ctx => {
   const n = parseInt(ctx.messageBody, 10)
 
   if (n) {
-    console.log(n)
+    console.log(`service1 received message "${n}"`)
     ctx.reply('//sqlssb/demo_message', n * 2)
   }
 })
 
 service2.on('//sqlssb/demo_message', ctx => {
-  console.log(ctx.messageBody)
   const n = parseInt(ctx.messageBody, 10)
 
-  if (n > 10) {
+  if (n >= 16) {
     ctx.reply('//sqlssb/demo_message', 'STOP')
     service2.stop()
     return
   }
 
   if (n) {
-    console.log(n)
+    console.log(`service2 received message "${n}"`)
     ctx.reply('//sqlssb/demo_message', n * 2)
   }
 })
@@ -39,7 +35,7 @@ Promise.all([
   service1.start(),
   service2.start()
 ]).then(() => {
-  return service1.send('sqlssb_demo_service_2', '//sqlssb/demo_message', 1)
+  return service1.send('sqlssb_demo_service_2', '//sqlssb/demo_message', 2)
 }).catch(err => {
   console.error(err)
 })
